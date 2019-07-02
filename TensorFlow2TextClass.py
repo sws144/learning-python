f47297d52812418d3229ff83a25c88af93626f3b
Stanley Wang <s.wang4331@gmail.com>
2019-07-02 06:45:12 -0400

    2text plot

--- TensorFlow2TextClass.py
+++ TensorFlow2TextClass.py
@@ -9,10 +9,14 @@ import sys
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
+# check for install
+install("tensorflow")
+install("numpy")
+install("matplotlib")
+
 # import relevant packages
 import tensorflow as tf 
 from tensorflow import keras 
-
 import numpy as np
 
 print("tensorflow version: " + tf.__version__)
@@ -68,3 +72,56 @@ model.add(keras.layers.Dense(16,activation=tf.nn.relu))
 model.add(keras.layers.Dense(1,activation=tf.nn.sigmoid))
 
 model.summary()
+
+# Add loss function to model
+model.compile(
+            optimizer = 'adam',
+            loss =  'binary_crossentropy', 
+            metrics=['acc']
+            )
+
+# Create a validation set to finetune model
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+# Train model
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs = 40,
+                    batch_size = 512,
+                    validation_data = (x_val,y_val),
+                    verbose=1
+                    )
+
+# Evaluate model
+results = model.evaluate(test_data, test_labels)
+
+print(results)
+
+# Graph of accuracy and history over time for training
+# import plotting package
+import matplotlib.pyplot as plt 
+
+history_dict = history.history
+history_dict.keys()
+
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1,len(acc)+1)
+
+# "bo" is for blue dot
+plt.plot(epochs, loss, 'bo', label = 'Training Loss')
+# b is for solid blue line
+plt.plot(epochs, val_loss, 'b', label = 'Validation Loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show(block=False)
