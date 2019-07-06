62a95ecff5120157d92d95f0bf243e1387794feb
Stanley Wang <s.wang4331@gmail.com>
2019-07-06 00:46:16 -0400

    finish h2o

--- LearnH2O.py
+++ LearnH2O.py
@@ -1,5 +1,7 @@
 # Learning H2O
 # https://h2o-release.s3.amazonaws.com/h2o/rel-ueno/2/docs-website/h2o-docs/booklets/GLMBooklet.pdf
+# http://h2o-release.s3.amazonaws.com/h2o/rel-xia/4/docs-website/h2o-docs/booklets/GLMBooklet.pdf
+
 
 # code to auto add packages
 import subprocess
@@ -79,7 +81,7 @@ h2o_df.summary()
 
 # choosing model
 # L-BGFS for larger # of predictors
-# IRLSM fewer predixtors
+# IRLSM fewer predictors
 
 
 # stopping criteria
@@ -171,5 +173,47 @@ h2o_df['CAPSULE'] = h2o_df['CAPSULE'].asfactor()
 
 rand_vec = h2o_df.runif(1234)
 
+train = h2o_df[rand_vec <= 0.8]
+valid = h2o_df[(rand_vec > 0.8) & (rand_vec <= 0.9)]
+test = h2o_df[rand_vec > 0.9]
 
+binomial_fit = H2OGeneralizedLinearEstimator(family = "binomial")
+binomial_fit.train(y="CAPSULE", x= ["AGE", "RACE", "PSA", "GLEASON"], training_frame = train, 
+    validation_frame = valid)
 
+#make & export predictions
+pred = binomial_fit.predict(test)
+h2o.export_file(pred, "pred.csv", force=True)
+# or you can export prediction to hdfs:
+# h2o.exportFile(pred, "hdfs://namenode/path/to/file.csv")
+
+# calculate metrics
+binomial_fit.model_performance(test)
+
+# remove response column inorder to test
+# use threshold for max than f1
+newdata = test
+newdata['CAPSULE'] = None
+newpred = binomial_fit.predict(newdata)
+newpred
+
+# manually define threshold for predictions to 0.3
+import pandas as pd
+pred = binomial_fit.predict(h2o_df)
+pred['predict'] = pred['p1'] > 0.3
+
+# POJO visualization object
+h2o_df = h2o.import_file("http://h2o-public-test-data.s3.amazonaws.com/smalldata/prostate/prostate.csv")
+h2o_df['CAPSULE'] = h2o_df['CAPSULE'].asfactor()
+binomial_fit = H2OGeneralizedLinearEstimator(family ="binomial")
+binomial_fit.train(y = "CAPSULE", x = ["AGE", "RACE","PSA", "GLEASON"], training_frame = h2o_df)
+h2o.download_pojo(binomial_fit)
+
+# Verifying model results
+h2o_df = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/airlines/allyears2k_headers.zip")
+model = H2OGeneralizedLinearEstimator(family = "binomial", nfolds = 5)
+model.train(y = "IsDepDelayed", x = ["Year", "Origin"], training_frame=h2o_df)
+print("full model training auc:", model.auc())
+print("full model validation auc:", model.auc(xval=True))
+for model_ in model.get_xval_models():
+    print(model_.model_id, "training auc:", model_.auc(), "validation auc:", model_.auc(valid=True))
