7bd029de60bb1978b4fe8792eda30c6b2919d33c
Stanley Wang <s.wang4331@gmail.com>
2019-07-14 22:30:27 -0400

    sklearn2

--- learning-ml-packages.py
+++ learning-ml-packages.py
@@ -71,4 +71,16 @@ vv.imshow(img_tint_resize, 2)
 # scikit-learn
 # classical ml algorithms 
 
+install("sklearn")
+
 # sample Decision Tree Classifier
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+
+# load iris datasets
+dataset = datasets.load_iris()
+
+# fit a CART model to data
+model = DecisionTreeClassifier()
+model.fit =(dataset.data, dataset.target)
